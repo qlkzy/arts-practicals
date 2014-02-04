cc5b2336ee9f7c050db0e6575279e86f6ad68ae6
David Morris <davidrowlandmorris@gmail.com>
2014-02-04 16:04:56 +0000

    Exercise 9

---
+++ exercises/ex9.adb
@@ -0,0 +1,31 @@
+with Ada.Text_Io;
+use Ada.Text_Io;
+
+procedure Ex9 is
+   
+   procedure Infinite_Work is
+      F : Duration := 0.0;
+   begin
+      Put_Line("Working");
+      loop
+         for J in 1 .. 10000000 loop
+            F := F + Duration(J * 10.0);
+         end loop;
+         Put_Line("Still Working");
+      end loop;
+   end Infinite_Work;
+   
+   task Worker;
+   
+   task body Worker is
+   begin
+      select
+         delay 1.0;
+      then abort
+         Infinite_Work;
+      end select;
+   end Worker;
+   
+begin
+   null;
+end Ex9;
