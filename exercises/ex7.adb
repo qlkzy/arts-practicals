bf4ab3630fde73c60613dcffe7f546bf17898fa4
David Morris <davidrowlandmorris@gmail.com>
2014-02-04 13:56:07 +0000

    Exercise 7

--- exercises/ex7.adb
+++ exercises/ex7.adb
@@ -2,14 +2,24 @@ with Ada.Text_Io;
 use Ada.Text_Io;
 with Ada.Real_Time;
 use Ada.Real_Time;
+with System;
+use System;
 
 procedure Ex7 is
-   
-   task Periodic;
-   task Background;
-   
+
+   Start_Time : Time := Clock + Seconds(1);
+
+   task Periodic is
+      pragma Priority(System.Priority'First + 5);
+   end Periodic;
+
+   task Background is
+      pragma Priority(System.Priority'First);
+   end;
+
    task body Periodic is
    begin
+      delay until Start_Time;
       for I in 1 .. 10 loop
          delay until Clock + Milliseconds(100);
          Put_Line("Periodic task execution " & Integer'Image(I));
@@ -17,8 +27,16 @@ procedure Ex7 is
    end Periodic;
 
    task body Background is
+      F : Duration := 0.0;
    begin
-      null;
+      delay until Start_Time;
+      Put_Line("Starting background computation");
+      for I in 1 .. 60 loop
+         Put_Line("Executing background computation");
+         for J in 1 .. 10000000 loop
+            F := F + Duration(J * 10.0);
+         end loop;
+      end loop;
    end Background;
 
 begin
